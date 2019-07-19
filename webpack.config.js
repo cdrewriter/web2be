ade4db4147dc967189bc1e06bcca3d5aae25132d
Dmitriy Agapov <pr-web2006@yandex.ru>
2019-07-19 23:49:02 +0500

    First page

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  context: __dirname,
+  devtool: "source-map",
+  entry: "./js/profile.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  module:{
+    loaders: [
+      {test : /\.css$/, loader: 'style!css!'}
+    ]
+  },
+  devServer: {
+    inline:true,
+    port: 10000
+  },
+}
+
